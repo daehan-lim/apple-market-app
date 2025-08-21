a3a5e3db42b5fc087d3a5ccc267616708102d3a3
daehan-lim <penjan.eng@gmail.com>
2025-08-21 16:37:23 +0900

    refactor(formatter): refactor formatKrw function into NumberFormatter class

--- lib/core/utils/number_formatter.dart
+++ lib/core/utils/number_formatter.dart
@@ -1,6 +1,8 @@
 import 'package:intl/intl.dart';
 
-String formatKrw(int number) {
-  final formatCurrency = NumberFormat.currency(locale: 'ko_KR', symbol: '');
-  return number > 0 ? '${formatCurrency.format(number)}원' : '0원(무료)';
+class NumberFormatter {
+  static String formatKrw(int number) {
+    final formatCurrency = NumberFormat.currency(locale: 'ko_KR', symbol: '');
+    return number > 0 ? '${formatCurrency.format(number)}원' : '0원(무료)';
+  }
 }
